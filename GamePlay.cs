f376186e2b3bd10e27c9fdd433d021a7db661872
Chao Yang <wamengyang1@yahoo.com>
2019-09-26 07:49:54 -0500

    first commit to project plus one class

---
+++ GamePlay.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSPLSproject
+{
+    class GamePlay
+    {
+
+        //member variable(has a)
+
+
+        //constructor(spawner)
+
+
+        //member method(can do)
+    }
+}
